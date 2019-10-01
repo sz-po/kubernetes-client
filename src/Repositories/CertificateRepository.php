0622f0217e5952496ca42d2019d8b6f3dfd08067
sz__po <2962046+sz-po@users.noreply.github.com>
2019-10-01 21:34:59 +0000

    Initial support for certmanager.k8s.io api.

--- src/Repositories/CertificateRepository.php
+++ src/Repositories/CertificateRepository.php
@@ -1,13 +1,17 @@
 <?php namespace Maclof\Kubernetes\Repositories;
 
 use Maclof\Kubernetes\Collections\CertificateCollection;
+use Maclof\Kubernetes\Repositories\Strategy\PatchMergeTrait;
 
 class CertificateRepository extends Repository
 {
+    use PatchMergeTrait;
+
     protected $uri = 'certificates';
 
     protected function createCollection($response)
     {
         return new CertificateCollection($response['items']);
     }
+
 }
--- src/Repositories/IssuerRepository.php
+++ src/Repositories/IssuerRepository.php
@@ -1,13 +1,17 @@
 <?php namespace Maclof\Kubernetes\Repositories;
 
 use Maclof\Kubernetes\Collections\IssuerCollection;
+use Maclof\Kubernetes\Repositories\Strategy\PatchMergeTrait;
 
 class IssuerRepository extends Repository
 {
+    use PatchMergeTrait;
+
     protected $uri = 'issuers';
 
     protected function createCollection($response)
     {
         return new IssuerCollection($response['items']);
     }
+
 }
