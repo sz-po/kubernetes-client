3ac08d08a8aa123c3c95f91eee06a4dbd6b763ce
sz__po <2962046+sz-po@users.noreply.github.com>
2019-10-04 00:43:06 +0000

    changed strategy for etcdcluster object

--- src/Repositories/EtcdClusterRepository.php
+++ src/Repositories/EtcdClusterRepository.php
@@ -1,9 +1,11 @@
 <?php namespace Maclof\Kubernetes\Repositories;
 
 use Maclof\Kubernetes\Collections\EtcdClusterCollection;
+use Maclof\Kubernetes\Repositories\Strategy\PatchMergeTrait;
 
 class EtcdClusterRepository extends Repository
 {
+    use PatchMergeTrait;
 
     protected $uri = 'etcdclusters';
 
